f870c8428b3aba8224f00118a3f0ded02d2afb78
Leighton Jenkins <leightonjenkins84@gmail.com>
2021-03-13 15:50:25 -0600

    updated script.js with more concise functionality and detailed comments, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# 05 Third-Party APIs: Work Day Scheduler
+# Work Day Scheduler
+
+This project is now live at https://ld-jenkins.github.io/scheduler/ !
+
+
+
+## Project Origins
+
+From humble origins, this project came with some starter html and css and a note reading "Some Assembly Required".
+
+![Starter Code](./assets/images/starter.png)
+
+
+
+## Design/Functionality
+
+With some tender love and css, Bootstrap grid column layout, and some JS the project came to life.
+The page now shows a multi-row style hour block layout running from 9AM to 5PM where a user may enter text in any hour block and click the adjacent save button to save that information within the browser.
+
+If the user closes and re-opens the page any information entered will re-populate the approriate text fields where it may be further edited.
+
+The top of the page also displays the current date and time.
+
+![Finished Code](./assets/images/finished.png)
--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,24 @@ var currTime = document.getElementById("currentDay");
 var blockTextAreaArr = document.getElementsByTagName("textarea");
 var buttonArr = document.getElementsByTagName("button");
 
+// this is not entirely necessary to the functionality of this app and is moreso pre-planning in case this was implemented to
+// display, for example, a 24-hour format instead of 9 to 5. This could be hard-coded on lines 17 and 19 but if I instead
+// wanted to display all 24-hours of a day I could simply changed the value of startingHour to 0 in one place and keep functionality
 var startingHour = 9;
 
+// using setInterval to update the current date and time on the screen every 1000ms or 1 second
 setInterval(function(){currTime.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');}, 1000);
 
 var currHour = moment().hour();
 
 for (var i = 0; i < blockTextAreaArr.length; i++) {
 
+    // grabbing the strings from local storage by index (since they were named in local storage by their index) and populating the textarea's
+    // with the relevant information.
     blockTextAreaArr[i].textContent = localStorage.getItem(i);
 
+    // setting the hour block color based on if each block's hour designation is less than or greater than, else equal to the current hour
+    // note: will only update colors on page refresh
     if ((i + startingHour) < currHour) {
         blockTextAreaArr[i].classList.add("past");
     } else if ((i + startingHour) > currHour) {
@@ -21,14 +29,54 @@ for (var i = 0; i < blockTextAreaArr.length; i++) {
     }
 }
 
-for( var i = 0; i < buttonArr.length; i++ ){
-    (function(i){
-        buttonArr[i].addEventListener('click', function(event) {
-            // console.log(event.target);
-            // console.log(i);
-            var textAreaSib = event.target.previousElementSibling;
-            // console.log(textAreaSib.value);
-            localStorage.setItem(i, textAreaSib.value);
-        }, false);
-    })(i);
-}
+// -----------------------------------------------------------------------------------
+// The next step is to add event listeners to each button that is used to save the text
+// the user has entered into the adjacent textarea. Since these are the only buttons on
+// the page I could simply loop through all buttons and add an event listener to each one.
+// I could have added an id to each button like "btn1", "btn2", "btn3", etc., then grabbed
+// the id using event.target.id and used that as the key for each local storage entry but
+// instead wanted to just use the index (the value of i) that I'm currently on in my for
+// loop to identify each string I wanted to save into local storage.
+
+// Below is my original approach but on line 51 instead of 'let index = i;' it read
+// 'var index = i;'. The issue with this is that due to the scoping of 'var' the variable
+// 'index' will update on each loop meaning when a user ends up clicking any button, the
+// index that would be read in would always be the last value 'i' was set to, which would be
+// 8 in this case. So here we have to use 'let' which has what's called block scope so that
+// the value of 'i' is captured in 'index' on each iteration of the for loop
+// -----------------------------------------------------------------------------------
+
+// for(var i = 0; i < buttonArr.length; i++){
+    
+//     let index = i;
+//     buttonArr[i].addEventListener('click', function(event){
+
+//         var textAreaSib = event.target.previousElementSibling;
+//         localStorage.setItem(index, textAreaSib.value);
+
+//     });
+// }
+
+// -----------------------------------------------------------------------------------
+// Now to get fancy, I updated this to use forEach() and nested arrow functions!
+// First, since the right hand expression on line 2 returns what's called an 'array-like object'
+// called an HTMLCollection, we must use 'Array.from()' to turn this into an array so
+// we can use 'forEach()' on it. On each loop in 'button' we capture a reference to the
+// HTML DOM button element we are currently on and in 'index' we capture a reference to the
+// current index we are on. The rest of this is just like any other time we would add an
+// event listener other than using another arrow function.
+// -----------------------------------------------------------------------------------
+
+Array.from(buttonArr).forEach((button, index) => {
+    button.addEventListener("click", event => {
+
+        // create a reference to event(the user's click).target(the DOM element the click event was triggered on).previousElementSibling(the element sitting directly before the target element)
+        var textAreaSib = event.target.previousElementSibling;
+        // add an item to local storage with the key as the current index in the loop and the value as the current value of textAreaSib (the text entered by the user in the adjacent textarea)
+        localStorage.setItem(index, textAreaSib.value);
+
+        // un-comment these two lines if you would like to see what the taget DOM element looks like and what index is given for each button
+        // console.log(event.target);
+        // console.log(index);
+    })
+})
